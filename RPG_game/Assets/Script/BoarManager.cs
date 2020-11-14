3541a399220de2bfd815d7f1b7c840b9663282e6
heatflat1021 <heatflat0407@gmail.com>
2020-11-14 18:06:26 +0900

    AIの基本を実装

---
+++ RPG_game/Assets/Script/BoarManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BoarManager : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+    }
+
+    void Update()
+    {
+        agent.destination = target.position;
+    }
+}
