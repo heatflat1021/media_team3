8cb936617bab891e957dded3633ce88defc85eb0
heatflat1021 <heatflat0407@gmail.com>
2020-12-06 20:08:58 +0900

    hpの数値を分数で表示されるように修正

--- RPG_game/Assets/Script/PlayerUIManager.cs
+++ RPG_game/Assets/Script/PlayerUIManager.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class PlayerUIManager: MonoBehaviour
 {
     public Slider hpSlider;
+    public Text hpText;
 
    
     public void UpdateHP(int hp)
     {
         hpSlider.value = hp;
+        hpText.text = ((int)(hp / hpSlider.maxValue * 16000)).ToString();
     }
 }
