e19a274a1c3f45a51aa736e511001272e70ba9ec
heatflat1021 <heatflat0407@gmail.com>
2020-12-10 20:45:13 +0900

    RecordManagerのリファクタリング

--- pattern_recognition_system/RecordManager.py
+++ pattern_recognition_system/RecordManager.py
@@ -5,8 +5,11 @@ import os
 from cortex import Cortex
 import EmotivInfo
 
+MESUREMENT_SECONDS = 20
+MESUREMENT_TIMES = 10
+
 def ready_confirm():
-    print('準備ができたらEnterキーを押してください。')
+    print("準備ができたらEnterキーを押してください。")
     s = input()
 
 def count_down(second):
@@ -15,7 +18,7 @@ def count_down(second):
         time.sleep(1)
 
 def isascii(s):
-    return re.compile(r'^[!-~]+$').match(s) is not None
+    return re.compile(r"^[!-~]+$").match(s) is not None
 
 class Command:
     def __init__(self, name, instruction, mesurement_times):
@@ -28,109 +31,82 @@ class Record():
 		self.c = Cortex(user, debug_mode=False)
 		self.c.do_prepare_steps()
 
-	def create_record_then_export(self,
-								record_name,
-								record_description,
-								record_length_s,
-								record_export_folder,
-								record_export_data_types,
-								record_export_format,
-								record_export_version):
-		
-		self.c.create_record(record_name,
-							record_description)
-
-		self.wait(record_length_s)
-
-		self.c.stop_record()
-
-		self.c.disconnect_headset()
-
-		self.c.export_record(record_export_folder,
-							record_export_data_types,
-							record_export_format,
-							record_export_version,
-							[self.c.record_id])
-
-
 	def wait(self, record_length_s):
-		print('start recording -------------------------')
+		print("start recording -------------------------")
 		length = 0
 		while length < record_length_s:
-			print('recording at {0} s'.format(length))
+			print("recording at {0} s".format(length))
 			time.sleep(1)
 			length+=1
-		print('end recording -------------------------')
+		print("end recording -------------------------")
 
 commands = [
-    Command('neutral', '平常心をイメージしてください。', 10),
-    Command('straight', '直進をイメージしてください。', 10),
-    Command('sword', '剣で攻撃するイメージをしてください。', 10),
-    Command('magic1', '火炎を放出するイメージをしてください。', 10),
-    Command('magic2', '岩を動かすイメージをしてください。', 10)
+    Command("neutral", "平常心をイメージしてください。", MESUREMENT_TIMES),
+    Command("straight", "直進をイメージしてください。", MESUREMENT_TIMES),
+    Command("sword", "剣で攻撃するイメージをしてください。", MESUREMENT_TIMES),
+    Command("magic1", "火炎を放出するイメージをしてください。", MESUREMENT_TIMES),
+    Command("magic2", "岩を動かすイメージをしてください。", MESUREMENT_TIMES)
 ]
 
 user = EmotivInfo.user
 
 r = Record(user)
 
-MESUREMENT_SECOND = 20
-
 ###############################################
 
-username = ''
+username = ""
 while True:
-    print('ユーザー名を入力してください')
-    print('ユーザー名には英数字及び記号が使えますが、日本語文字は使えません。')
-    print('ユーザー名:', end='')
+    print("ユーザー名を入力してください")
+    print("ユーザー名には英数字及び記号が使えますが、日本語文字は使えません。")
+    print("ユーザー名:", end="")
     username = input()
 
     if not isascii(username):
-        print('! ユーザー名に日本語文字は使えません。')
+        print("! ユーザー名に日本語文字は使えません。")
         continue
 
     current_dir = os.getcwd()
     files = os.listdir(current_dir)
     dir_list = [f for f in files if os.path.isdir(os.path.join(current_dir, f))]
     if username in dir_list:
-        print('ユーザー{}は既に存在します。別のユーザー名で新しくユーザーを登録する場合はMを、そのユーザーで続きから計測をする場合はそれ以外の入力をしてください。'.format(username))
+        print("ユーザー{}は既に存在します。別のユーザー名で新しくユーザーを登録する場合はMを、そのユーザーで続きから計測をする場合はそれ以外の入力をしてください。".format(username))
     else:
         print("ユーザー名は{}で間違いないですか。修正する場合はMを、間違い無い場合はそれ以外の入力をしてください。".format(username))
-    print('入力:', end='')
+    print("入力:", end="")
     s = input()
-    if(s in 'mM'):
+    if(s in "mM"):
         continue
 
     if not username in dir_list:
         os.mkdir(os.path.join(current_dir, username))
     break
 
-print('計測秒数や計測回数を指定しますか？指定する場合はMを、指定しない場合はそれ以外を入力してください。')
-print('入力:', end='')
+print("計測秒数と計測回数を指定しますか？指定する場合はMを、指定しない場合はそれ以外を入力してください。")
+print("入力:", end="")
 s = input()
-if(s in 'mM'):
-    recordTime = recordNum = 0
+if(s in "mM"):
+    record_seconds = record_num = 0
     while True:
-        print("計測秒数:", end='')
+        print("計測秒数:", end="")
         n = input()
         try:
-            recordTime = int(n)
+            record_seconds = int(n)
         except:
             continue
         break
 
     while True:
-        print("計測回数:", end='')
+        print("計測回数:", end="")
         n = input()
         try:
-            recordNum = int(n)
+            record_num = int(n)
         except:
             continue
         break
 
-    MESUREMENT_SECOND = recordTime
+    MESUREMENT_SECONDS = record_seconds
     for command in commands:
-        command.mesurement_times = recordNum
+        command.mesurement_times = record_num
 
 print("計測を開始します。")
 ready_confirm()
@@ -139,7 +115,7 @@ record_id_list = []
 for idx, command in enumerate(commands):
     print("{}番目のコマンド[{}]の計測を開始します。".format((idx + 1), command.name))
     print("計測中は{}".format(command.instruction))
-    print("{}秒間の計測を{}セット行います。".format(MESUREMENT_SECOND, command.mesurement_times))
+    print("{}秒間の計測を{}セット行います。".format(MESUREMENT_SECONDS, command.mesurement_times))
 
     for i in range(command.mesurement_times):
         ready_confirm()
@@ -151,7 +127,7 @@ for idx, command in enumerate(commands):
         record_description = ''
 
         r.c.create_record(record_name, record_description)
-        r.wait(MESUREMENT_SECOND)
+        r.wait(MESUREMENT_SECONDS)
         r.c.stop_record()
         record_id_list.append(r.c.record_id)
 
@@ -159,9 +135,9 @@ for idx, command in enumerate(commands):
 
 # export parameters
 record_export_folder = os.path.join(current_dir, username)
-record_export_data_types = ['EEG', 'MOTION']
-record_export_format = 'CSV'
-record_export_version = 'V2'
+record_export_data_types = ["EEG", "MOTION"]
+record_export_format = "CSV"
+record_export_version = "V2"
 
 # export EEG data to CSV
 r.c.disconnect_headset()
@@ -173,7 +149,7 @@ r.c.export_record(record_export_folder,
 
 # 不要なJSONファイルの削除
 data_files = os.listdir(record_export_folder)
-json_files = [s for s in data_files if '.json' in s]
+json_files = [s for s in data_files if ".json" in s]
 for json_file in json_files:
     os.remove('./{}/{}'.format(username, json_file))
 
